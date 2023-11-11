73f979d77b29b4f7b2e75766cb089ab40e16d856
Klayton Tan <s10242375@connect.np.edu.sg>
2023-11-11 22:30:12 +0800

    Models

---
+++ PFDASG1/Models/CreditCard.cs
@@ -0,0 +1,11 @@
+﻿namespace PFDASG1.Models
+{
+    public class CreditCard
+    {
+        public int creditCardId { get; set; }
+        public int accountId { get; set; }
+        public string cardNumber { get; set; }
+        public DateTime expirationDate { get; set; }
+        public string cvv { get; set; }
+    }
+}
