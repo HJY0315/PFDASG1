f0f67c262e1496ef911a67457d844714dd1cab03
rei-shell <s10247620@connect.np.edu.sg>
2023-11-14 21:50:02 +0800

    did some stuff on the view all card page and added the transactiondal

---
+++ PFDASG1/DAL/TransactionsDAL.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.SqlClient;
+
+namespace PFDASG1.DAL
+{
+    public class TransactionsDAL
+    {
+        private IConfiguration Configuration { get; }
+        private SqlConnection conn;
+
+        public TransactionsDAL()
+        {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+            string strConn = Configuration.GetConnectionString("OCBCConnectionString");
+            conn = new SqlConnection(strConn);
+        }
+
+
+    }
+}
---
+++ PFDASG1/Models/userlogin.cs
@@ -0,0 +1,11 @@
+﻿namespace PFDASG1.Models
+{
+    public class userlogin
+    {
+        public int Userid { get; set; }
+        public string Name { get; set; }
+        public char AccessCode { get; set; }
+        public char phoneNumber { get; set; }
+        public char Pin { get; set; }
+    }
+}
