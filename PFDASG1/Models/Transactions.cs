ce4a5d352cc8cd777b3d4e4bd24f6b2b2a63db41
rei-shell <s10247620@connect.np.edu.sg>
2023-11-15 12:16:47 +0800

    70% done for the transaction page

--- PFDASG1/Models/Transactions.cs
+++ PFDASG1/Models/Transactions.cs
@@ -1,10 +1,16 @@
-﻿namespace PFDASG1.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace PFDASG1.Models
 {
     public class Transactions
     {
         public int transactionId { get; set; }
         public int accountId { get; set; }
         public string description {get; set;}
+
+        [Display(Name = "Amount")]
+        [Required(ErrorMessage = "Please enter a City!")]
         public decimal amount { get; set; }
         public DateTime transactionDate { get; set; }
         public int receiverId { get; set; }
