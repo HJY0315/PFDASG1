a8b09b74c5f06b9aa7a952f293897b2220620bbc
Klayton Tan <s10242375@connect.np.edu.sg>
2023-11-15 13:41:38 +0800

    Model validation

--- PFDASG1/Models/SecurityQuestion.cs
+++ PFDASG1/Models/SecurityQuestion.cs
@@ -1,10 +1,24 @@
-﻿namespace PFDASG1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PFDASG1.Models
 {
     public class SecurityQuestion
     {
+        [Required(ErrorMessage = "Security question ID is required.")]
+        [Display(Name = "Security Question ID")]
         public int SQId { get; set; }
-        public int accountID { get; set; }
-        public string question { get; set; }
+
+        [Required(ErrorMessage = "Account ID is required.")]
+        [Display(Name = "Account ID")]
+        public int accountId { get; set; }
+
+        [Required(ErrorMessage = "Question ID is required.")]
+        [Display(Name = "Question ID")]
+        public int questionId { get; set; }
+
+        [Required(ErrorMessage = "Security question answer is required.")]
+        [Display(Name = "Security Question Answer")]
+        [StringLength(100, ErrorMessage = "Security question answer cannot exceed 100 characters.")]
         public string answer { get; set; }
     }
 }
--- PFDASG1/Models/User.cs
+++ PFDASG1/Models/User.cs
@@ -4,13 +4,34 @@ namespace PFDASG1.Models
 {
     public class User
     {
+        [Required(ErrorMessage = "User ID is required.")]
+        [Display(Name = "User ID")] // Display name for Userid property
         public int Userid { get; set; }
-        public string Name { get; set; }
-        public string AccessCode { get; set; }
-        public string phoneNumber { get; set; }
-        [Required(ErrorMessage = "Pin is required.")]
 
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(50, ErrorMessage = "Name cannot exceed 50 characters.")]
+        [Display(Name = "Full Name")] // Display name for Name property
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Access code is required.")]
+        [StringLength(7, ErrorMessage = "Access code must be 7 characters long.")]
+        [Display(Name = "Access Code")] // Display name for AccessCode property
+        public string AccessCode { get; set; }
+
+        [Required(ErrorMessage = "Phone number is required.")]
+        [RegularExpression(@"^\+65\d{8}$", ErrorMessage = "Phone number must be in the format +65XXXXXXXX.")]
+        [Display(Name = "Phone Number")] // Display name for phoneNumber property
+        public string phoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Pin is required.")]
+        [StringLength(6, ErrorMessage = "Pin must be 6 characters long.")]
+        [Display(Name = "PIN")] // Display name for Pin property
         public string Pin { get; set; }
+
+        [Required(ErrorMessage = "NRIC is required.")]
+        [StringLength(9, ErrorMessage = "NRIC must be 9 characters long.")]
+        [RegularExpression(@"^[S|T]\d{7}[A-Z]$", ErrorMessage = "NRIC must be in the format S/T followed by 7 digits and an alphabet.")]
+        [Display(Name = "NRIC")] // Display name for NRIC property
         public string NRIC { get; set; }
     }
 }
