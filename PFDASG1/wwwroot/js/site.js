f7e99f268f2e7e9804a126c4ca09e8bba10e280e
HJY0315 <117621210+HJY0315@users.noreply.github.com>
2023-11-15 21:08:05 +0800

    Add font enlarge to the page after the accessibility mode is on. But some stylling is not align and need to be adjust.

--- PFDASG1/wwwroot/js/site.js
+++ PFDASG1/wwwroot/js/site.js
@@ -2,3 +2,25 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function toggleEnlargedText() {
+    // Assuming 'enlarged-text' is the class for enlarged text
+    document.body.classList.toggle('enlarged-text');
+
+    // Store the user's preference in localStorage
+    const isEnlarged = document.body.classList.contains('enlarged-text');
+    localStorage.setItem('enlargedText', isEnlarged);
+}
+
+function applyUserPreference() {
+    const isEnlarged = localStorage.getItem('enlargedText') === 'true';
+
+    // Toggle the class based on the stored preference
+    if (isEnlarged) {
+        document.body.classList.add('enlarged-text');
+    } else {
+        document.body.classList.remove('enlarged-text');
+    }
+}
+
+document.addEventListener('DOMContentLoaded', applyUserPreference);
