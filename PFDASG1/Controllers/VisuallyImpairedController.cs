61b78fba3b255368cccdc82b614cde9891b470cc
Klayton Tan <s10242375@connect.np.edu.sg>
2023-11-16 09:19:58 +0800

    transaction progress

--- PFDASG1/Controllers/VisuallyImpairedController.cs
+++ PFDASG1/Controllers/VisuallyImpairedController.cs
@@ -11,14 +11,50 @@ namespace PFDASG1.Controllers
         TransactionsDAL TransactionsContext = new TransactionsDAL();
         User user;
 
+
         public IActionResult Settings()
         {
             return View();
         }
-    
+
+        
+
+
+        
+
+        [HttpGet]
         public IActionResult Transfer()
         {
             return View();
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Transfer(Transactions transaction)
+        {
+            ModelState.Remove("TransactionDate");
+            ModelState.Remove("NRIC");
+            ModelState.Remove("MobileNumber");
+            ModelState.Remove("AccountId");
+
+            int userId = (int)HttpContext.Session.GetInt32("id");
+            if (ModelState.IsValid)
+            {
+                //transaction = new Transactions();
+                TransactionsContext.Add(transaction, userId);
+                TempData["message"] = "Money has successfully been transferred";
+                TempData["status"] = "success";
+
+                return RedirectToAction("Index"); // Redirect to a success page
+            }
+            else
+            {
+
+                return View(transaction);
+            }
+            // If the model state is not valid, return to the form with validation errors
+
         }
 
         public IActionResult ViewAllCards()
@@ -35,7 +71,7 @@ namespace PFDASG1.Controllers
             List<Transactions> transactions = TransactionsContext.getalltransactions(Id);
             return View(transactions);
         }
-  
+
 
         public IActionResult Search()
         {
--- PFDASG1/Models/Transactions.cs
+++ PFDASG1/Models/Transactions.cs
@@ -7,34 +7,36 @@ namespace PFDASG1.Models
     {
         [Required(ErrorMessage = "Transaction ID is required.")]
         [Display(Name = "Transaction ID")]
-        public int TransactionId{ get; set; }
-
-
-        [Display(Name = "Description")]
-        [StringLength(255, ErrorMessage = "Description cannot exceed 255 characters.")]
-        public string? Description { get; set; }
+        public int TransactionId { get; set; }
 
         [Required(ErrorMessage = "Account ID is required.")]
         [Display(Name = "Account ID")]
         public int AccountId { get; set; }
+
+        [Required(ErrorMessage = "Recipient ID is required.")]
+        [Display(Name = "Recipient ID")]
+        public int RecipientID { get; set; }
+
+        [Display(Name = "Sender ID")]
+        public int SenderID { get; set; }
+
+        [Display(Name = "NRIC")]
+        [StringLength(9, ErrorMessage = "NRIC cannot exceed 9 characters.")]
+        public string NRIC { get; set; }
+
+        [Display(Name = "Mobile Number")]
+        [StringLength(8, ErrorMessage = "Mobile Number cannot exceed 8 characters.")]
+        public string MobileNumber { get; set; }
 
         [Required(ErrorMessage = "Amount is required.")]
         [Display(Name = "Amount")]
         [Range(-9999999999.99, 9999999999.99, ErrorMessage = "Amount must be between -9999999999.99 and 9999999999.99.")]
         public decimal Amount { get; set; }
 
-        [Required(ErrorMessage = "Transaction date is required.")]
-        [Display(Name = "Transaction Date")]
+        [Display(Name = "Description")]
+        [StringLength(255, ErrorMessage = "Description cannot exceed 255 characters.")]
+        public string Description { get; set; }
+
         public DateTime TransactionDate { get; set; }
-
-        [Required(ErrorMessage = "Recipient ID is required.")]
-
-        [Display(Name = "Recipient ID")]
-        public int RecipientID { get; set; }
-        [Required(ErrorMessage = "Sender ID is required.")]
-
-        [Display(Name = "Sender ID")]
-        public int SenderID { get; set; }
-
     }
 }
